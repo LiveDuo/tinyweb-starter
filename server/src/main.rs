f4dc4262782ba8662a67f5e9663d2e4540d2348e
Andreas Tzionis <andy0n7@gmail.com>
2024-09-25 08:09:29 +0300

    added tinyhttp

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,17 @@
 
+use tiny_http::{Server, Response};
+
 // cargo run -p server
 fn main() {
-    println!("server");
+
+    
+    let server = Server::http("0.0.0.0:4000").unwrap();
+    println!("Listening on port {:?}", server.server_addr().port());
+
+    for request in server.incoming_requests() {
+        println!("{:?} {:?}", request.method(), request.url());
+
+        let response = Response::from_string("hello world");
+        request.respond(response).unwrap();
+    }
 }
