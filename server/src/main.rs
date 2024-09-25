0705c9aa0b343ad95c5bb66d67e7c0418c4e57da
Andreas Tzionis <andy0n7@gmail.com>
2024-09-25 09:05:17 +0300

    minor fix

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,11 +13,18 @@ fn main() {
 
         match (request.method().as_str(), request.url()) {
             ("GET", "/") => {
+                // TODO read from file
                 let header = "Content-type: text/html".parse::<Header>().unwrap();
                 let response = Response::from_string("<b>hello world</b>");
                 request.respond(response.with_header(header)).unwrap();
             },
-            ("GET", "/ping") => {
+            ("GET", "/assets/client.wasm") => {
+                // TODO read from file
+                let header = "Content-type: text/html".parse::<Header>().unwrap();
+                let response = Response::from_string("<b>hello world</b>");
+                request.respond(response.with_header(header)).unwrap();
+            },
+            ("GET", "/api/ping") => {
                 let header: Header = "Content-type: application/json".parse::<Header>().unwrap();
                 let message = json::object!{ pong: true };
                 let response = Response::from_string(message.dump());
