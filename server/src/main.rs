1a70415dec1b616da4ea39c75e7a1d07f56c1412
Andreas Tzionis <andy0n7@gmail.com>
2024-10-02 22:39:05 +0300

    worked on text file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 
-use std::process::Command;
+use std::io::{Read, Write};
+use std::{fs::OpenOptions, process::Command};
 use std::path::PathBuf;
 
 use tiny_http::{Server, Header, Response};
@@ -17,7 +18,9 @@ fn main() {
     let server = Server::http("0.0.0.0:8000").unwrap();
     println!("Listening on port {:?}", server.server_addr().port());
 
-    for request in server.incoming_requests() {
+    let mut file = OpenOptions::new().create(true).read(true).write(true).open("/tmp/data").unwrap();
+
+    for mut request in server.incoming_requests() {
         println!("{:?} {:?}", request.method(), request.url());
 
         match (request.method().as_str(), request.url()) {
@@ -33,6 +36,108 @@ fn main() {
                 let header = "Content-type: application/wasm".parse::<Header>().unwrap();
                 let response = Response::from_data(data);
                 request.respond(response.with_header(header)).unwrap();
+            },
+            (_, _) if request.url().starts_with("/api/todo") => {
+
+                let header: Header = "Content-type: application/json".parse::<Header>().unwrap();
+
+                let mut body = String::new();
+                request.as_reader().read_to_string(&mut body).unwrap();
+                
+                match request.method().as_str() {
+                    "GET" => {
+
+                        let id_opt = request.url().split("/").nth(3);
+                        if id_opt.is_none() {
+                            let response = Response::from_string("Invalid parameter");
+                            request.respond(response).unwrap();
+                            return;
+                        }
+
+                        let mut file_data = String::new();
+                        file.read_to_string(&mut file_data).unwrap();
+
+                        let id = id_opt.unwrap().parse::<usize>().unwrap();
+                        let line_opt = file_data.split("\n").nth(id);
+                        if line_opt.is_none() {
+                            let response = Response::from_string("Line error");
+                            request.respond(response).unwrap();
+                            return;
+                        }
+                        
+                        let message = json::object!{ data: line_opt.unwrap() };
+                        let response = Response::from_string(message.dump());
+                        request.respond(response.with_header(header)).unwrap();
+                    },
+                    "POST" => {
+
+                        let mut file_data = String::new();
+                        file.read_to_string(&mut file_data).unwrap();
+
+                        let mut lines = file_data.split("\n").collect::<Vec<_>>();
+                        lines.push("data");
+                        std::fs::write("/tmp/data", lines.join("\n")).unwrap();
+                        
+                        let message = json::object!{ sucess: true };
+                        let response = Response::from_string(message.dump());
+                        request.respond(response.with_header(header)).unwrap();
+
+                    },
+                    "PUT" => {
+                        
+                        let id_opt = request.url().split("/").nth(3);
+                        if id_opt.is_none() {
+                            let response = Response::from_string("Invalid parameter");
+                            request.respond(response).unwrap();
+                            return;
+                        }
+
+                        let mut file_data = String::new();
+                        file.read_to_string(&mut file_data).unwrap();
+
+                        let id = id_opt.unwrap().parse::<usize>().unwrap();
+                        let mut lines = file_data.split("\n").collect::<Vec<_>>();
+                        if id > lines.len() {
+                            let response = Response::from_string("File error");
+                            request.respond(response).unwrap();
+                            return;
+                        }
+
+                        lines[id] = body.as_str();
+                        file.write(lines.join("\n").as_bytes()).unwrap();
+                        
+                        let message = json::object!{ sucess: true };
+                        let response = Response::from_string(message.dump());
+                        request.respond(response.with_header(header)).unwrap();
+
+                    },
+                    "DELETE" => {
+
+                        let id_opt = request.url().split("/").nth(3);
+                        if id_opt.is_none() {
+                            let response = Response::from_string("Invalid parameter");
+                            request.respond(response).unwrap();
+                            return;
+                        }
+
+                        let mut file_data = String::new();
+                        file.read_to_string(&mut file_data).unwrap();
+
+                        let id = id_opt.unwrap().parse::<usize>().unwrap();
+                        let mut lines = file_data.split("\n").collect::<Vec<_>>();
+                        lines.remove(id);
+                        file.write(lines.join("\n").as_bytes()).unwrap();
+                        
+                        let message = json::object!{ sucess: true };
+                        let response = Response::from_string(message.dump());
+                        request.respond(response.with_header(header)).unwrap();
+
+                    },
+                    _ => {
+                        let response = Response::from_string("Invalid request");
+                        request.respond(response).unwrap();
+                    }
+                }
             },
             ("GET", "/api/ping") => {
                 let header: Header = "Content-type: application/json".parse::<Header>().unwrap();
